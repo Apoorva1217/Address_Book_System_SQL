6b82f03054b405a81ea15d3665ef8781ff924efa
Apoorva1217 <apoorvarasal12@gmail.com>
2020-11-20 20:02:33 +0530

    [Apoorva] Add . Ability to insert new Contacts to Address Book

--- AddressBook.sql
+++ AddressBook.sql
@@ -13,3 +13,13 @@ Zip_Code numeric(6) not null,
 Phone_Number numeric(10) unique not null,
 Email VARCHAR(50), CHECK(Email LIKE '%_@__%.__%' AND PATINDEX('%[^a-z,^A-Z,0-9,@,.,_,\-]%', Email)=0)
 );
+
+--UC3 Ability to insert new Contacts to Address Book
+INSERT INTO Address_Book(First_Name,Last_Name,Person_Address,City,State,Zip_Code,Phone_Number,Email)
+VALUES('Apoorva','Rasal','ChandanNagar','Pune','Maharashtra',411001,9876543212,'Appu12@gmail.com'),
+	  ('Swara','Shinde','Kalwa','Thane','Maharashtra',400601,8076543444,'Swara22@gmail.com'),
+	  ('Yash','Rasal','Kharadi','Pune','Maharashtra',411201,8976543212,'Yash17@gmail.com'),
+	  ('Manaswi','Nalawade','Bandra','Mumbai','Maharashtra',401721,9776543423,'Manu26@gmail.com'),
+	  ('Aayush','Kadam','Panvel','NaviMumbai','Maharashtra',420601,8071243444,'AayushK212@gmail.com');
+	  
+SELECT * FROM Address_Book;
